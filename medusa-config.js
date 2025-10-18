8ab6f42fee3f04299db26977576afe42aea9803f
Amir-HM <amir@Amirs-MacBook-Pro.local>
2025-10-18 14:14:49 +0200

    Fix medusa-config.js: remove TypeScript non-null assertions, update env usage

--- medusa-config.js
+++ medusa-config.js
@@ -21,9 +21,9 @@ module.exports = defineConfig({
         }
       : {},
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret"
     }
